55c57a1e7ad6b1ddf03eecaa66d10a9f903163f9
Rasmus Nilsson <rasmus.nilsson@brightstraining.com>
2023-03-03 14:50:22 +0100

    Added band list & co

---
+++ BandsMvc/Models/DataService.cs
@@ -0,0 +1,21 @@
+﻿using BandsMvc.Models;
+
+namespace Acme.Models
+{
+    public class DataService // Service
+    {
+        // Fejk-DB
+        List<Band> bands = new List<Band>
+        {
+            new Band { Id = 1, Name = "Sebbes Angels", Description = "Rock Band"},
+            new Band { Id = 20, Name = "Ralles Jazz", Description = "Jazz Band"},
+            new Band { Id = 35, Name = "Oscar och Co", Description = "Orkester"},
+        };
+
+        public Band[] GetAllBands()
+        {
+            return bands.OrderBy(x => x.Id).ToArray();
+        }
+
+    }
+}
