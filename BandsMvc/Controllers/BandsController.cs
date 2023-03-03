fa9bd691c392abb9f8c849402df4a073bba37415
Rasmus Nilsson <rasmus.nilsson@brightstraining.com>
2023-03-03 15:11:36 +0100

    Rasmus last push

--- BandsMvc/Controllers/BandsController.cs
+++ BandsMvc/Controllers/BandsController.cs
@@ -8,11 +8,19 @@ namespace BandsMvc.Controllers
     public class BandsController : Controller
     {
         DataService dataService = new DataService();
-        [HttpGet("")]
+        [HttpGet("")]       
         public IActionResult Index()
         {
             Band[] model = dataService.GetAllBands();
             return View(model);
+        }
+
+        [HttpGet("details/{id}")]
+        [HttpGet("details/{id}")]
+        [HttpGet("details/{id}")]
+        public IActionResult Details(int id)
+        {
+            return View();
         }
     }
 }
