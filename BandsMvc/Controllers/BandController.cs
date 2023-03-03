8aca1e1e63bffc573a46e6cf2027f8212b6635cd
Sebas <Sebas@LAPTOP-TKOO3441>
2023-03-03 14:29:47 +0100

    Add project files.

---
+++ BandsMvc/Controllers/BandController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BandsMvc.Controllers
+{
+    public class BandController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
