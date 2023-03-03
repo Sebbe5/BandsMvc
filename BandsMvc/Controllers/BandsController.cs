a4a450da6daf070eaac56da3d8c4f8fed49ac4c4
Rasmus Nilsson <rasmus.nilsson@brightstraining.com>
2023-03-03 14:59:28 +0100

    Update controllers & Views

--- BandsMvc/Controllers/BandsController.cs
+++ BandsMvc/Controllers/BandsController.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Acme.Models;
+using BandsMvc.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BandsMvc.Controllers
 {
+
     public class BandsController : Controller
     {
+        DataService dataService = new DataService();
+        [HttpGet("")]
         public IActionResult Index()
         {
+            Band[] model = dataService.GetAllBands();
             return View();
         }
     }
